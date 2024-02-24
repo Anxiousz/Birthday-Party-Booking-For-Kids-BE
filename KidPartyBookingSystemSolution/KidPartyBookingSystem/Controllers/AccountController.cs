13f5b7bbd8398c533ce54907d72afe081b59ec43
DESKTOP-K0TQQAP\anh12 <anh123456bhh@gmail.com>
2024-02-24 16:06:05 +0700

    [DuyAn] [Set up] : set up structure code

---
+++ KidPartyBookingSystemSolution/KidPartyBookingSystem/Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using BusinessObjects;
+using Microsoft.AspNetCore.Mvc;
+using Services;
+
+namespace KidPartyBookingSystem.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AccountController : ControllerBase
+    {
+        private IAccountService _accountService;
+        public AccountController(IAccountService accountService)
+        {
+            _accountService = accountService;
+        }
+
+        [HttpGet]
+        public IActionResult getAllAccounts()
+        {
+            List<RegisteredUser> registeredUsers = _accountService.GetRegisteredUserAccount();
+            return Ok(registeredUsers);
+        }
+    }
+}

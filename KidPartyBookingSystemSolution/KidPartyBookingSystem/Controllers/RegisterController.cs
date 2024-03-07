8b30e22e96c188e4902ef6de30cf2dcd475f10c3
DESKTOP-K0TQQAP\anh12 <anh123456bhh@gmail.com>
2024-03-07 19:04:06 +0700

    [Duy An] [Register] : Add Register function

---
+++ KidPartyBookingSystemSolution/KidPartyBookingSystem/Controllers/RegisterController.cs
@@ -0,0 +1,55 @@
+﻿using BusinessObjects.Request;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using Services;
+
+namespace KidPartyBookingSystem.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    [AllowAnonymous]
+    [EnableCors]
+    public class RegisterController : ControllerBase
+    {
+        private IRegisteredUserService _registeredUserService;
+        private IPartyHostService _partyHostService;
+        public RegisterController(IRegisteredUserService registeredUserService, IPartyHostService partyHostService)
+        {
+            _registeredUserService = registeredUserService;
+            _partyHostService = partyHostService;
+        }
+
+        [HttpPost("RegisteredUser")]
+        public IActionResult CreateRegisteredUser([FromBody] RequestRegisteredUserDTO request)
+        {
+            if (request == null)
+            {
+                return BadRequest("The field not empty");
+            }
+            bool checkExisted = _registeredUserService.checkRegisteredUserExistedByEmail(request.Email.Trim());
+            if (checkExisted != true)
+            {
+                RequestRegisteredUserDTO createAccount = _registeredUserService.CreateRegisteredUser(request);
+                return Ok(createAccount);
+            }
+            return Conflict("The user is existed");
+        }
+
+        [HttpPost("PartyHost")]
+        public IActionResult CreatePartyHost([FromBody] RequestPartyHostDTO request)
+        {
+            if (request == null)
+            {
+                return BadRequest("The field not empty");
+            }
+            bool checkExisted = _partyHostService.checkPartyHostExistedByEmail(request.Email.Trim());
+            if (checkExisted != true)
+            {
+                RequestPartyHostDTO createAccount = _partyHostService.CreatePartyHost(request);
+                return Ok(createAccount);
+            }
+            return Conflict("The user is existed");
+        }
+    }
+}
